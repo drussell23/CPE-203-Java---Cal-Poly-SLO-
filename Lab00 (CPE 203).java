ea304712b0d21947833025f8bea699cb346071e6
drussell23 <djamesr23@gmail.com>
2020-09-17 23:08:33 -0700

    Add files via upload

---
+++ Lab00 (CPE 203).java
@@ -0,0 +1,45 @@
+public class Lab00
+{
+	public static void main(String[] args)
+	{
+		// Declaring and initializing some variables.
+		int x = 5;
+		String y = "hello";
+		double z = 9.8;
+		
+		// Printing the variables. 
+		System.out.println("x: " + x + " y: " + y + " z: " + z);
+		
+		// A list (make an array in Java).
+		int [] numbers = {3, 6, -1, 2};
+		
+		for(int count = 0; count < numbers.length; count++)
+		{
+			System.out.println(numbers[count]);
+		}
+		
+		// Call a function. 
+		System.out.println("Found: ");
+		Count_Character(y, 'l');
+		
+		for(int j = 1; j < 11; j++)
+		{
+			System.out.print(j + " ");
+		}
+		System.out.println();
+	}
+	public static void Count_Character(String str, char c_temp)
+	{
+		int temp = 0;
+		
+		// This calculates the length of a string.
+		for(int i = 0; i < str.length(); i++)
+		{
+			if(str.charAt(i) == c_temp)
+			{
+				temp++;
+			}
+		}
+		System.out.println(temp);
+	}
+}
