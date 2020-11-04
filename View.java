8d58cb62c182a7ffc7ed185ffe0ea0b81f2b5dcb
drussell23 <djamesr23@gmail.com>
2020-11-03 18:47:56 -0800

    Add files via upload

---
+++ View.java
@@ -0,0 +1,22 @@
+public class View{
+    //private int sessionID;
+    private String productID;
+    private int priceCost;
+
+
+    public View( String productID, String priceCost){
+        //this.sessionID = Integer.parseInt(sessionID);
+        this.productID = productID;
+        this.priceCost = Integer.parseInt(priceCost);
+    }
+
+    public void setProduct(String productID){this.productID = productID;}
+    public String getProduct(){return productID;}
+
+    public void setPriceCost(int print){this.priceCost = priceCost;}
+    public int getPriceCost(){return this.priceCost;}
+
+
+
+
+}
